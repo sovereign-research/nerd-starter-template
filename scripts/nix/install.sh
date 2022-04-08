cc4fb8543be7708a19e683780d13981a36f09b9f
Kyle Derby MacInnis <director@sovereign.enterprises>
2022-04-07 20:09:50 -0600

    fixing legacy ssl

--- scripts/nix/install.sh
+++ scripts/nix/install.sh
@@ -13,17 +13,19 @@
 # \*                                            */
 
 # Read .ENV Variables
-declare -x BUILD_PATH=$(pwd)\..
+declare -x BUILD_PATH=$(pwd)
 
 cd $BUILD_PATH
 
 source $BUILD_PATH/.env
 
 # Compile
-docker-compose build 
+docker-compose build --no-cache
 
 # Deploy
 docker-compose up -d
+
+cd $BUILD_PATH/server
 
 # Migrate DB
 yarn
